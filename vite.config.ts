f4db76340c1f8d7fa0a0ceaf1aeeb3867eb1d06b
dsivanaa <ds.ivanavieira@gmail.com>
2025-04-24 16:01:42 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
-
-import { defineConfig } from 'vite';
-import hydrogen from '@shopify/hydrogen/plugin';
+// vite.config.ts
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [hydrogen()],
-  ssr: {
-    external: ['isbot'],
-  },
-});
+  root: './',
+  build: {
+    outDir: 'dist'
+  }
+})
